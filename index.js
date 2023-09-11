b6e9bddc4dd4b9f557818996bdb5ea4bef8b8d0e
Gabriel LR <dayta.scientist@gmail.com>
2023-09-11 19:33:47 -0300

    Install faker

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { faker } = require('@faker-js/faker')
 const express = require('express')
 const app = express()
 
@@ -8,10 +9,52 @@ app.get('/', (req, res) => {
 })
 
 app.get('/products', (req, res) => {
-  res.json({
-    name: 'Product 1',
-    price: 1000
-  })
+  const products = []
+  for (let index = 0; index < 50; index++) {
+    products.push(
+      {
+        name: faker.commerce.product(),
+        price: parseInt(faker.commerce.price({ symbol: '$' }), 10),
+        image: faker.image.url()
+      }
+    )
+  }
+  res.json(products)
+})
+
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params
+  res.json(
+    {
+      id,
+      name: 'Product 2',
+      price: 2000
+    }
+  )
+})
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params
+  res.json(
+    {
+      categoryId,
+      productId
+    }
+  )
+})
+
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query
+  if (limit && offset) {
+    res.json(
+      {
+        limit,
+        offset
+      }
+    )
+  } else {
+    res.send('There are no parameters')
+  }
 })
 
 app.listen(port, () => {
