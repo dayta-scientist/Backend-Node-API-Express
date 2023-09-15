4f8019fee48831c11b29a5f818997c87090af3c3
Gabriel LR <dayta.scientist@gmail.com>
2023-09-15 17:19:15 -0300

    Add cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const routerApi = require('./routes')
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler')
@@ -7,6 +8,18 @@ const app = express()
 const port = process.env.PORT || 1234
 
 app.use(express.json())
+
+const whitelist = ['http://localhost:5500', 'https://myapp.co']
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('No permitido'))
+    }
+  }
+}
+app.use(cors(options))
 
 app.get('/', (req, res) => {
   res.send({ message: 'Mi first server' })
